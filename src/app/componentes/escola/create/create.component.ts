cf4ea7a50dc75efdb0013e5d0bf9654faa868dcd
Gabriel Martins <gabriel.rodrigo.mars@gmail.com>
2021-02-25 20:58:59 -0300

    Início da implementação do cadastro de escola

--- src/app/componentes/escola/create/create.component.ts
+++ src/app/componentes/escola/create/create.component.ts
@@ -1,3 +1,8 @@
+import { User } from 'src/app/models/user.model';
+import { Escola } from './../../../models/escola.model';
+import { EscolaService } from './../../../services/escola.service';
+import { ToastService } from './../../../services/toast.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  escola: Escola = {
+    codigoMec: null,
+    nome: '',
+    email: '',
+    telefone: ''
+  }
+
+  user: User;
+  formsOk: Boolean;
+
+  constructor(
+    private router: Router,
+    private toastService: ToastService,
+    private escolaService: EscolaService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  createEscola(): void {
+    this.formsOk = this.validateFields();
+
+    if(this.formsOk) {
+      this.user = JSON.parse(sessionStorage.getItem('usuario'));
+      this.escolaService.cadastrarEscola(this.escola, this.user.hash).subscribe(() => {
+        this.toastService.showMessage('Escola cadastrada!', true);
+        this.router.navigate(['/escola']);
+      });
+    } else {
+      this.toastService.showMessage('Preencha todos os campos.', false);
+    }
+  }
+
+  // Valida se os campos do formulário estão vazios
+  validateFields(): Boolean {
+    if(this.escola.nome === null || this.escola.nome == null ||
+      this.escola.email === '' || this.escola.email == null ||
+      this.escola.telefone === '' || this.escola.telefone == null || this.escola.codigoMec == null) {
+        return false;
+    } else {
+      return true;
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/escola']);
+  }
 }
